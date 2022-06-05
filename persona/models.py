41762571a18bb57d00822c74ed54fc206c10fcf3
vanemanza1 <vanesa@infra-ThinkPad-E470>
2022-06-05 19:15:07 -0300

    creo modelo hobby

--- persona/models.py
+++ persona/models.py
@@ -1,6 +1,16 @@
 #from model_utils.models import TimeStampedModel
 from django.db import models
 
+class Hobby(models.Model):
+    hobby = models.CharField("Pasatiempo", max_length=50)
+
+    class Meta:
+        verbose_name = "Hobby"
+        verbose_name_plural = "Hobbies"
+
+    def __str__(self):
+        return self.hobby    
+
 class Person(models.Model):
     """  Modelo para registrar personas de una agenda  """
 
@@ -8,6 +18,7 @@ class Person(models.Model):
     job = models.CharField('Trabajo', max_length=30, blank=True)
     email = models.EmailField(blank=True, null=True)
     phone = models.CharField('telefono', max_length=15, blank=True)
+    hobbies = models.ManyToManyField(Hobby)
 
 
     class Meta:
@@ -16,3 +27,17 @@ class Person(models.Model):
     
     def __str__(self):
         return self.full_name
+
+class Reunion(models.Model):
+    """ Modelo para reunion """
+
+    persona = models.ForeignKey(Person, on_delete=models.CASCADE)
+    fecha = models.DateField()
+    asunto = models.CharField("Asunto", max_length=100)
+
+    class Meta:
+        verbose_name = "Reunion"
+        verbose_name_plural = "Reuniones"
+
+    def __str__(self):
+        return self.asunto            
